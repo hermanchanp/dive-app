6af10ab3143c130ef600e52cb63a149221a71cc6
hermanchanp <dr.herman.chan@gmail.com>
2024-04-18 10:05:52 +0100

    Add spinner

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import updateDivers from './queries/UpdateDivers';
 
 function App() {
   const [divers, setDivers] = useState(DefaultDivers); 
+  const [isLoading, setIsLoading] = useState(true); 
 
 
   const diveCount = () => {
@@ -42,15 +43,14 @@ function App() {
 
   useEffect(() => {
     getDivers().then((data: any) => {
-      // console.log('Warmup')
-      // console.log(data)
       const yesterday = isYesterday(data.lastUpdated)
-      // console.log(yesterday)
+
       if (yesterday) {
         setDivers(DefaultDivers);
       }else{
         setDivers(data.data)
       }
+      setIsLoading(false)
     })
   }, []);
 
@@ -95,47 +95,106 @@ function App() {
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-5xl">
         Are we diving tonight?
       </h1>
-      {
-        diveCount() > 2 && <>
-          <h1 className="pt-4 pb-8 text-7xl md:text-5xl text-green-500">
-            Yes
-          </h1>
-        </>
-      }
-      {
-        diveCount() == 2 && <>
-          <h1 className="pt-4 pb-8 text-7xl  md:text-5xl text-orange-500">
-            Maybe
-          </h1>
-        </>
-      }
-      {
-        diveCount() <= 1  && <>
-          <h1 className="pt-4 pb-8  text-7xl  md:text-5xl text-red-500">
-            No
-          </h1>
-        </>
-      }
-            {
-        diveCount() >= 2 && 
-        <>
-        Diving at: {findClosestTime()}
-        </>
-      }
-      
-      <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
-        <div className="flex justify-between items-center mb-4">
+      {isLoading ? <div className="spinner"> </div> :
+      <>
+        {
+          diveCount() > 2 && <>
+            <h1 className="pt-4 pb-8 text-7xl md:text-5xl text-green-500">
+              Yes
+            </h1>
+          </>
+        }
+        {
+          diveCount() == 2 && <>
+            <h1 className="pt-4 pb-8 text-7xl  md:text-5xl text-orange-500">
+              Maybe
+            </h1>
+          </>
+        }
+        {
+          diveCount() <= 1  && <>
+            <h1 className="pt-4 pb-8  text-7xl  md:text-5xl text-red-500">
+              No
+            </h1>
+          </>
+        }
+              {
+          diveCount() >= 2 && 
+          <>
+          Diving at: {findClosestTime()}
+          </>
+        }
+        
+        <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
+          <div className="flex justify-between items-center mb-4">
 
-        </div>
-        <div className="flex flex-col space-y-4">
-          {
-            divers.map((diver) => (
-              <>
-                {
-                  diver.alignLeft && (
-                    <div className="flex flex-row">
-                      <div>
-                          <img
+          </div>
+          <div className="flex flex-col space-y-4">
+            {
+              divers.map((diver) => (
+                <>
+                  {
+                    diver.alignLeft && (
+                      <div className="flex flex-row">
+                        <div>
+                            <img
+                              className="rounded-2xl border border-blue-800"
+                              src={`/${diver.id}.png`}
+                              alt={diver.name}
+                              width={130}
+                              height={167}
+                            />
+                        </div>
+                        <div className="p-2 flex flex-col  gap-y-2">
+                          <div className="font-bold text-2xl">{diver.name}</div>
+                          <div>
+                          <span className="pr-2">Diving:</span>
+                          <button
+                            onClick={() => toggleReactComp(diver.id, diver.available)}
+                              className={`relative inline-flex items-center h-6 rounded-full w-11 ${diver.available ? 'bg-green-600' : 'bg-red-600'}`}
+                            >
+                              <span
+                                className={`inline-block w-4 h-4 transform bg-white rounded-full ${diver.available ? 'translate-x-6' : 'translate-x-1'}`}
+                              />
+                              
+                          </button>
+                            <span className="pl-2">{diver.available ? "yes": "no"}</span>
+                          </div>
+                          <div className="pl-2">
+                            <label htmlFor="time">Select time:</label>
+                            <input disabled={!diver.available} type="time" id="time" name="time" value={diver.availableFrom} onChange={(e) => handleTimeChange(diver.id, e)} />
+                          </div>
+                          <div className="pt-4 text-xs text-gray-600">Last updated: {diver.updatedAt.toLocaleString()}</div>
+                        </div>
+                      </div>
+                    )
+                  }
+                  
+                  {
+                    !diver.alignLeft && (
+                      <div className="flex flex-row">
+                        <div className="p-2 flex-grow flex flex-col text-right gap-y-2">
+                        <div className="font-bold text-2xl">{diver.name}</div>
+                          <div>
+                          <span className="pr-2">Diving:</span>
+                          <button
+                            onClick={() => toggleReactComp(diver.id, diver.available)}
+                              className={`relative inline-flex items-center h-6 rounded-full w-11 ${diver.available ? 'bg-green-600' : 'bg-red-600'}`}
+                            >
+                              <span
+                                className={`inline-block w-4 h-4 transform bg-white rounded-full ${diver.available ? 'translate-x-6' : 'translate-x-1'}`}
+                              />
+                              
+                          </button>
+                            <span className="pl-2">{diver.available ? "yes": "no"}</span>
+                          </div>
+                          <div className="pl-2">
+                            <label htmlFor="time">Select time:</label>
+                            <input disabled={!diver.available} type="time" id="time" name="time" value={diver.availableFrom} onChange={(e) => handleTimeChange(diver.id, e)} />
+                          </div>
+                          <div className="pt-4 text-xs text-gray-600">Last updated: {diver.updatedAt.toLocaleString()}</div>
+                        </div>
+                        <img
                             className="rounded-2xl border border-blue-800"
                             src={`/${diver.id}.png`}
                             alt={diver.name}
@@ -143,70 +202,15 @@ function App() {
                             height={167}
                           />
                       </div>
-                      <div className="p-2 flex flex-col  gap-y-2">
-                        <div className="font-bold text-2xl">{diver.name}</div>
-                        <div>
-                        <span className="pr-2">Diving:</span>
-                        <button
-                          onClick={() => toggleReactComp(diver.id, diver.available)}
-                            className={`relative inline-flex items-center h-6 rounded-full w-11 ${diver.available ? 'bg-green-600' : 'bg-red-600'}`}
-                          >
-                            <span
-                              className={`inline-block w-4 h-4 transform bg-white rounded-full ${diver.available ? 'translate-x-6' : 'translate-x-1'}`}
-                            />
-                            
-                        </button>
-                          <span className="pl-2">{diver.available ? "yes": "no"}</span>
-                        </div>
-                        <div className="pl-2">
-                          <label htmlFor="time">Select time:</label>
-                          <input disabled={!diver.available} type="time" id="time" name="time" value={diver.availableFrom} onChange={(e) => handleTimeChange(diver.id, e)} />
-                        </div>
-                        <div className="pt-4 text-xs text-gray-600">Last updated: {diver.updatedAt.toLocaleString()}</div>
-                      </div>
-                    </div>
-                  )
-                }
-                
-                {
-                  !diver.alignLeft && (
-                    <div className="flex flex-row">
-                      <div className="p-2 flex-grow flex flex-col text-right gap-y-2">
-                      <div className="font-bold text-2xl">{diver.name}</div>
-                        <div>
-                        <span className="pr-2">Diving:</span>
-                        <button
-                          onClick={() => toggleReactComp(diver.id, diver.available)}
-                            className={`relative inline-flex items-center h-6 rounded-full w-11 ${diver.available ? 'bg-green-600' : 'bg-red-600'}`}
-                          >
-                            <span
-                              className={`inline-block w-4 h-4 transform bg-white rounded-full ${diver.available ? 'translate-x-6' : 'translate-x-1'}`}
-                            />
-                            
-                        </button>
-                          <span className="pl-2">{diver.available ? "yes": "no"}</span>
-                        </div>
-                        <div className="pl-2">
-                          <label htmlFor="time">Select time:</label>
-                          <input disabled={!diver.available} type="time" id="time" name="time" value={diver.availableFrom} onChange={(e) => handleTimeChange(diver.id, e)} />
-                        </div>
-                        <div className="pt-4 text-xs text-gray-600">Last updated: {diver.updatedAt.toLocaleString()}</div>
-                      </div>
-                      <img
-                          className="rounded-2xl border border-blue-800"
-                          src={`/${diver.id}.png`}
-                          alt={diver.name}
-                          width={130}
-                          height={167}
-                        />
-                    </div>
-                  )
-                }
-              </>
-            ))
-          }
+                    )
+                  }
+                </>
+              ))
+            }
+          </div>
         </div>
-      </div>
+      </>
+      }
     </main>
   );
 }
